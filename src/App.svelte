5fb55f7a5767fe9c107948099325b5def8e1eb3d
Brad Garropy <bradgarropy@gmail.com>
2020-07-11 23:52:46 -0500

    timer progress.

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,26 @@
-<h1>🤖 cybernetically enhanced svelte starter</h1>
+<script>
+    import {format} from "date-fns"
+
+    let id
+    let start
+    let elapsed = 0
+
+    const startTimer = () => {
+        start = Date.now()
+        id = setInterval(() => (elapsed = Date.now() - start), 100)
+    }
+
+    const stopTimer = () => clearInterval(id)
+
+    const resetTimer = () => {
+        start = Date.now()
+        elapsed = 0
+    }
+</script>
+
+<div>
+    <pre>{format(elapsed, 'mm:ss:SS')}</pre>
+    <button on:click={startTimer}>START</button>
+    <button on:click={stopTimer}>STOP</button>
+    <button on:click={resetTimer}>RESET</button>
+</div>
