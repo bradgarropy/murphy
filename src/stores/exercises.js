a1eb5866ae1870daba69d42649a25413f053e71e
Brad Garropy <bradgarropy@gmail.com>
2020-07-18 00:03:19 -0500

    smarter exercise store.

--- src/stores/exercises.js
+++ src/stores/exercises.js
@@ -2,60 +2,47 @@ import {derived} from "svelte/store"
 
 import {runs, rounds} from "./config.js"
 
-const constructExercises = (runs, rounds) => {
+const createExercise = (name, round = null) => {
+    const exercise = {
+        name,
+        startTime: null,
+        endTime: null,
+        time: null,
+        round,
+    }
+
+    return exercise
+}
+
+const createRound = number => {
+    const exercises = ["push ups", "pull ups", "squats"]
+    const round = exercises.map(exercise => createExercise(exercise, number))
+
+    return round
+}
+
+const createExercises = (runs, rounds) => {
     const exercises = []
 
     if (runs) {
-        exercises.push({
-            name: "run",
-            startTime: null,
-            endTime: null,
-            time: null,
-            round: null,
-        })
+        exercises.push(createExercise("run"))
     }
 
     for (let index = 0; index < rounds; index++) {
-        exercises.push({
-            name: "push ups",
-            startTime: null,
-            endTime: null,
-            time: null,
-            round: index + 1,
-        })
-
-        exercises.push({
-            name: "pull ups",
-            startTime: null,
-            endTime: null,
-            time: null,
-            round: index + 1,
-        })
-
-        exercises.push({
-            name: "squats",
-            startTime: null,
-            endTime: null,
-            time: null,
-            round: index + 1,
-        })
+        createRound(index + 1).forEach(exercise => exercises.push(exercise))
     }
 
     if (runs) {
-        exercises.push({
-            name: "run",
-            startTime: null,
-            endTime: null,
-            time: null,
-            round: null,
-        })
+        exercises.push(createExercise("run"))
     }
 
     return exercises
 }
 
-const exercises = derived([runs, rounds], ([$runs, $rounds]) =>
-    constructExercises($runs, $rounds),
+const exercises = derived(
+    [runs, rounds],
+    ([$runs, $rounds]) => createExercises($runs, $rounds),
+    [],
 )
 
 export {exercises}
