a01f324c16f575a839e35e53a20a71944541aa5c
Brad Garropy <bradgarropy@gmail.com>
2020-08-07 22:06:13 -0500

    round detail animation.

--- src/components/Round.svelte
+++ src/components/Round.svelte
@@ -1,6 +1,8 @@
 <script>
     import {format} from "date-fns"
     import {writable} from "svelte/store"
+    import {cubicOut} from "svelte/easing"
+    import {slide} from "svelte/transition"
 
     import {workout} from "../stores/workout.js"
     import {exercises} from "../stores/exercises.js"
@@ -23,7 +25,7 @@
     }
 </script>
 
-<div class="grid grid-cols-2 text-xl uppercase" on:click={onClick}>
+<div class="grid grid-cols-2 text-xl uppercase font-body" on:click={onClick}>
     <span class="tracking-tighter font-bold">
         {getRoundLabel(round.number, $runs, $rounds, $exercises)}
     </span>
@@ -33,7 +35,9 @@
     </span>
 
     {#if $expanded}
-        <div class="grid grid-cols-2 text-xl uppercase col-span-2 py-2">
+        <div
+            class="grid grid-cols-2 text-xl uppercase col-span-2 py-2"
+            transition:slide={{duration: 500, easing: cubicOut}}>
             {#each workoutExercises as exercise}
                 <span class="tracking-tighter font-bold pl-4">
                     {exercise.name}
