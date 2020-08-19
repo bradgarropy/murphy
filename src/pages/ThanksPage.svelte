cf48bb9afbe6cd921e031f44ebccee7aaa0fcf71
Brad Garropy <bradgarropy@gmail.com>
2020-08-19 16:00:11 -0500

    update user data after payment.

--- src/pages/ThanksPage.svelte
+++ src/pages/ThanksPage.svelte
@@ -1,5 +1,16 @@
 <script>
     import {fade} from "svelte/transition"
+    import netlifyIdentity from "netlify-identity-widget"
+
+    import {user} from "../stores/user.js"
+
+    const refreshUser = async () => {
+        const currentUser = await netlifyIdentity.currentUser()
+        const userData = await currentUser.getUserData()
+        user.set(userData)
+    }
+
+    refreshUser()
 </script>
 
 <main in:fade={{duration: 500}}>
