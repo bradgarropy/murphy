b85be508359a388a6ea410acaa9789538ab58e00
Brad Garropy <bradgarropy@gmail.com>
2020-09-20 23:26:13 -0500

    lighthouse.

--- src/App.svelte
+++ src/App.svelte
@@ -20,6 +20,18 @@
     import "./utils/identity.js"
 </script>
 
+<svelte:head>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Rowdies:wght@300;400;700&display=swap"
+        rel="stylesheet"
+    />
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet"
+    />
+</svelte:head>
+
 <audio id="countdown" src="/countdown.wav">
     <track kind="captions" />
 </audio>
