eb0b5404a5de9131f372679e3b1a1f437da70886
Brad Garropy <bradgarropy@gmail.com>
2020-09-22 10:17:40 -0500

    get users.

--- functions/payment.js
+++ functions/payment.js
@@ -4,32 +4,39 @@ const stripe = require("./utils/stripe")
 
 const handler = async (event, context) => {
     const body = JSON.parse(event.body)
-    console.log(body)
+    const email = body.data.object.customer_email
+    const {url, token} = context.clientContext.identity
+
+    try {
+        // validate stripe webook
+        stripe.webhooks.constructEvent(
+            event.body,
+            event.headers["stripe-signature"],
+            process.env.STRIPE_WEBHOOK_SECRET,
+        )
+    } catch (err) {
+        return {
+            statusCode: 400,
+            body: `Webhook Error: ${err.message}`,
+        }
+    }
+
+    // only handle checkout completed events
+    if (body.type !== "checkout.session.completed") {
+        return {statusCode: 200}
+    }
+
+    const getUserResponse = await fetch(`${url}/admin/users`, {
+        method: "GET",
+        headers: {Authorization: `Bearer ${token}`},
+    })
+
+    // TODO: handle fetch error
+    const getUserData = await getUserResponse.json()
+    console.log(getUserData)
 
     const response = {statusCode: 200}
     return response
-
-    // const customerId = body.data.object.customer
-    // const {url, token} = context.clientContext.identity
-
-    // try {
-    //     // validate stripe webook
-    //     stripe.webhooks.constructEvent(
-    //         event.body,
-    //         event.headers["stripe-signature"],
-    //         process.env.STRIPE_WEBHOOK_SECRET,
-    //     )
-    // } catch (err) {
-    //     return {
-    //         statusCode: 400,
-    //         body: `Webhook Error: ${err.message}`,
-    //     }
-    // }
-
-    // // only handle checkout completed events
-    // if (body.type !== "checkout.session.completed") {
-    //     return {statusCode: 200}
-    // }
 
     // const userBody = {
     //     app_metadata: {
