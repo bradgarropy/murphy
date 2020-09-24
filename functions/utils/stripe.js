fdf5532999f60f138adcda927afe50b7e8e752b1
Brad Garropy <bradgarropy@gmail.com>
2020-09-24 09:26:54 -0500

    log env variables.

--- functions/utils/stripe.js
+++ functions/utils/stripe.js
@@ -1,9 +1,13 @@
 const Stripe = require("stripe")
 
+console.log(`CONTEXT: ${process.env.CONTEXT}`)
+console.log(`STRIPE_SK_TEST: ${process.env.STRIPE_SK_TEST}`)
+console.log(`STRIPE_SK_LIVE: ${process.env.STRIPE_SK_LIVE}`)
+
 const options = {
     maxNetworkRetries: 2,
 }
 
-const stripe = Stripe(process.env.STRIPE_SECRET_KEY, options)
+const stripe = Stripe(process.env.STRIPE_SK_LIVE, options)
 
 module.exports = stripe
